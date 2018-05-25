449f60c15a7add14f9d21f6e1c42e2a826482975
Mi Nombre <valeriadroguettl@gmail.com>
2018-05-25 14:23:54 -0400

    Corregí errores de tipeo en ambas funciones

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-function clickCifrar(){
+function clickCifrar() {
     //obtengo el desplazamiento
     var desplazar = document.getElementById("desplazamiento").value;
     //obtengo el mensaje
-    var mensaje =  document.getElementById("escribeMensaje").value;
+    var mensaje = document.getElementById("escribeMensaje").value;
     //Utilizo la libreria cipher para codificar el mensaje
     var mensajeCifrado = cipher.encode(mensaje, desplazar);
     //El mensaje encriptado lo muestro en el elemento "showMessage"
     document.getElementById("muestraMensaje").value = mensajeCifrado;
 
-  }
-  
-  function clickDescifrar(){
+}
+
+function clickDescifrar() {
     //obtengo el desplazamiento
     var desplazar = document.getElementById("desplazamiento").value;
     //obtengo el mensaje
-    var mensaje =  document.getElementById("escribeMensaje").value;
+    var mensaje = document.getElementById("escribeMensaje").value;
     //utilizo la libreria cipher para decodificar el mensaje
     var mensajeDescifrado = cipher.decode(mensaje, desplazar);
     //El mensaje encriptado lo muestro en el elemento "getMessage"
     document.getElementById("muestraMensaje").value = mensajeDescifrado;
 
-  }
+}
