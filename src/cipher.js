100a091bbd32903a1499b29ea61a9a8a5680fcfe
Mi Nombre <valeriadroguettl@gmail.com>
2018-05-25 09:26:41 -0400

    Modifiqué todos los archivos de src

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,46 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (cifrar, desplazar) => {
+    /* cifrar es el mensaje */
+    let longitudMensaje = cifrar.lenght - 1;
+    let letrasMensaje = [];
+    for (let index = 0; index <= longitudMensaje; index = index++) {
+        letrasMensaje [index] = cifrar.charCodeAt (index); /*aquí se guarda el número de cada caracter*/
+    }
+    let letrasCifradas = []; /*cree esta variable para guardar las letras cifradas*/
+    
+    for (let index = 0; index <= longitudMensaje; index = index++)
+    {
+      let ecuacionCesar = ((letrasMensaje[index] - 65 + desplazar) % 26 + 65);
+      letrasCifradas [index] = String.fromCharCode(ecuacionCesar); /*guardé las letras cifradas*/
+    }
+    let mensajeCifrado = ""; /*cree esta variable para guardar el mensaje cifrado*/
+    for (let index = 0; index <= longitudMensaje; index = index++) {
+      mensajeCifrado = mensajeCifrado + letrasCifradas [index]; /*guardé el mensaje cifrado*/
+    }
+
+    return mensajeCifrado; /*la función me devuelve el mensaje final cifrado para poder verlo*/
+  
   },
-  decode: () => {
-    /* Acá va tu código */
+  decode: (descifrar, desplazar) => {
+    /* Vamos a descifrar el mensaje */
+    let longitudMensaje = descifrar.lenght - 1;
+    let numeroCifrado = [];
+    for (let index = 0; index <= longitudMensaje; index = index++) {
+        numeroCifrado [index] = descifrar.charCodeAt (index); /*aquí se guarda el número de cada caracter*/
+    }
+    let letrasDescifradas = []; /*cree esta variable para guardar las letras descifradas*/
+    
+    for (let index = 0; index <= longitudMensaje; index = index++)
+    {
+      let ecuacionCesar = ((letrasMensaje[index] - 65 - desplazar) % 26 + 65);
+      letrasDescifradas [index] = String.fromCharCode(ecuacionCesar); /*guardé las letras descifradas*/
+    }
+    let mensajeDescifrado = ""; /*cree esta variable para guardar el mensaje descifrado*/
+    for (let index = 0; index <= longitudMensaje; index = index++) {
+      mensajeDescifrado = mensajeDescifrado + letrasDescifradas [index]; /*guardé el mensaje descifrado*/
+    }
+
+    return mensajeDescifrado; /*la función me devuelve el mensaje final descifrado para poder verlo*/
+  
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-/* Acá va tu código */
+function encode(){
+    //obtengo el mensaje
+    var cifrar =  document.getElementById("cajaTextoUno").value;
+    //obtengo el desplazamiento
+    var desplazar = document.getElementById("desplazamiento").value;
+    //obtengo el modo
+    var mood =  document.getElementById("botonElegir").value;
+    //Utilizo la libreria cipher para codificar el mensaje
+    var mensajeCifrado = cipher.encode(cifrar,desplazar);
+    //El mensaje encriptado lo muestro en el elemento "showMessage"
+    document.getElementById("cajaTextoDos").value = mensajeCifrado;
+
+  }
+  
+  function decode(){
+    //obtengo el mensaje
+    var descifrar =  document.getElementById("cajaTextoUno").value;
+    //obtengo el desplazamiento
+    var desplazar = document.getElementById("desplazamiento").value;
+    //obtengo el modo
+    var mood =  document.getElementById("botonElegir").value;
+    //utilizo la libreria cipher para decodificar el mensaje
+    var mensajeDescifrado = cipher.decode(cifrar,desplazar);
+    //El mensaje encriptado lo muestro en el elemento "getMessage"
+    document.getElementById("cajaTextoDos").value = mensajeDescifrado;
+  
+  }
+
+function myFunction() {
+    var x = document.getElementById("myRange").value;
+    document.getElementById("demo").innerHTML = x;
+}
