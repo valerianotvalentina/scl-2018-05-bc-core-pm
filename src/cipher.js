7a3283bdc2791338f320cc8cb553c7f4532e9db7
Mi Nombre <valeriadroguettl@gmail.com>
2018-05-25 17:35:19 -0400

    Hice unas pruebas pero dejé el código tal como antes

--- src/cipher.js
+++ src/cipher.js
@@ -1,47 +1,45 @@
 window.cipher = {
-  encode: (cifrar, desplazar) => {
-    /* cifrar es el mensaje */
-    let longitudMensaje = cifrar.length - 1;
-    let letrasMensaje = [];
-    for (let index = 0; index <= longitudMensaje; index++) {
-        letrasMensaje [index] = cifrar.charCodeAt(index); /*aquí se guarda el número de cada caracter*/
-    }
-    let letrasCifradas = []; /*cree esta variable para guardar las letras cifradas*/
-    
-    for (let index = 0; index <= longitudMensaje; index++)
-    {
-      let ecuacionCesar = ((letrasMensaje[index] - 65 + desplazar) % 26 + 65);
-      letrasCifradas [index] = String.fromCharCode(ecuacionCesar); /*guardé las letras cifradas*/
-    }
-    let mensajeCifrado = ""; /*cree esta variable para guardar el mensaje cifrado*/
-    for (let index = 0; index <= longitudMensaje; index++) {
-      mensajeCifrado = mensajeCifrado + letrasCifradas [index]; /*guardé el mensaje cifrado*/
-    }
+    encode: (cifrar, desplazar) => {
+        /* cifrar es el mensaje */
+        let longitudMensaje = cifrar.length - 1;
+        let letrasMensaje = [];
+        for (let index = 0; index <= longitudMensaje; index++) {
+            letrasMensaje[index] = cifrar.charCodeAt(index); /*aquí se guarda el número de cada caracter*/
+        }
+        let letrasCifradas = []; /*cree esta variable para guardar las letras cifradas*/
 
-    return mensajeCifrado; /*la función me devuelve el mensaje final cifrado para poder verlo*/
-  
-  },
+        for (let index = 0; index <= longitudMensaje; index++) {
+            let ecuacionCesar = ((letrasMensaje[index] - 65 + desplazar) % 26 + 65);
+            letrasCifradas[index] = String.fromCharCode(ecuacionCesar); /*guardé las letras cifradas*/
+        }
+        let mensajeCifrado = ""; /*cree esta variable para guardar el mensaje cifrado*/
+        for (let index = 0; index <= longitudMensaje; index++) {
+            mensajeCifrado = mensajeCifrado + letrasCifradas[index]; /*guardé el mensaje cifrado*/
+        }
 
-  decode: (descifrar, desplazar) => {
-    /* Vamos a descifrar el mensaje */
-    let longitudMensaje = descifrar.length - 1;
-    let letrasMensaje = [];
-    for (let index = 0; index <= longitudMensaje; index++) {
-      letrasMensaje [index] = descifrar.charCodeAt (index); /*aquí se guarda el número de cada caracter*/
-    }
-    let letrasDescifradas = []; /*cree esta variable para guardar las letras descifradas*/
-    
-    for (let index = 0; index <= longitudMensaje; index++)
-    {
-      let ecuacionCesar = ((letrasMensaje[index] - 65 - desplazar) % 26 + 65);
-      letrasDescifradas [index] = String.fromCharCode(ecuacionCesar); /*guardé las letras descifradas*/
-    }
-    let mensajeDescifrado = ""; /*cree esta variable para guardar el mensaje descifrado*/
-    for (let index = 0; index <= longitudMensaje; index++) {
-      mensajeDescifrado = mensajeDescifrado + letrasDescifradas [index]; /*guardé el mensaje descifrado*/
-    }
+        return mensajeCifrado; /*la función me devuelve el mensaje final cifrado para poder verlo*/
 
-    return mensajeDescifrado; /*la función me devuelve el mensaje final descifrado para poder verlo*/
-  
-  }
+    },
+
+    decode: (descifrar, desplazar) => {
+        /* Vamos a descifrar el mensaje */
+        let longitudMensaje = descifrar.length - 1;
+        let letrasMensaje = [];
+        for (let index = 0; index <= longitudMensaje; index++) {
+            letrasMensaje[index] = descifrar.charCodeAt(index); /*aquí se guarda el número de cada caracter*/
+        }
+        let letrasDescifradas = []; /*cree esta variable para guardar las letras descifradas*/
+
+        for (let index = 0; index <= longitudMensaje; index++) {
+            let ecuacionCesar = ((letrasMensaje[index] - 65 - desplazar) % 26 + 65);
+            letrasDescifradas[index] = String.fromCharCode(ecuacionCesar); /*guardé las letras descifradas*/
+        }
+        let mensajeDescifrado = ""; /*cree esta variable para guardar el mensaje descifrado*/
+        for (let index = 0; index <= longitudMensaje; index++) {
+            mensajeDescifrado = mensajeDescifrado + letrasDescifradas[index]; /*guardé el mensaje descifrado*/
+        }
+
+        return mensajeDescifrado; /*la función me devuelve el mensaje final descifrado para poder verlo*/
+
+    }
 }
