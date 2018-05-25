bc0acab4408a0b8e5fbc526a01fd1ec9ff9293e7
Mi Nombre <valeriadroguettl@gmail.com>
2018-05-25 14:12:43 -0400

    Escribí encode completo en cipher.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,23 @@
-function encode(){
-    //obtengo el mensaje
-    var cifrar =  document.getElementById("cajaTextoUno").value;
+function clickCifrar(){
     //obtengo el desplazamiento
     var desplazar = document.getElementById("desplazamiento").value;
-    //obtengo el modo
-    var mood =  document.getElementById("botonElegir").value;
+    //obtengo el mensaje
+    var mensaje =  document.getElementById("escribeMensaje").value;
     //Utilizo la libreria cipher para codificar el mensaje
-    var mensajeCifrado = cipher.encode(cifrar,desplazar);
+    var mensajeCifrado = cipher.encode(mensaje, desplazar);
     //El mensaje encriptado lo muestro en el elemento "showMessage"
-    document.getElementById("cajaTextoDos").value = mensajeCifrado;
+    document.getElementById("muestraMensaje").value = mensajeCifrado;
 
   }
   
-  function decode(){
-    //obtengo el mensaje
-    var descifrar =  document.getElementById("cajaTextoUno").value;
+  function clickDescifrar(){
     //obtengo el desplazamiento
     var desplazar = document.getElementById("desplazamiento").value;
-    //obtengo el modo
-    var mood =  document.getElementById("botonElegir").value;
+    //obtengo el mensaje
+    var mensaje =  document.getElementById("escribeMensaje").value;
     //utilizo la libreria cipher para decodificar el mensaje
-    var mensajeDescifrado = cipher.decode(cifrar,desplazar);
+    var mensajeDescifrado = cipher.decode(mensaje, desplazar);
     //El mensaje encriptado lo muestro en el elemento "getMessage"
-    document.getElementById("cajaTextoDos").value = mensajeDescifrado;
-  
-  }
+    document.getElementById("muestraMensaje").value = mensajeDescifrado;
 
-function myFunction() {
-    var x = document.getElementById("myRange").value;
-    document.getElementById("demo").innerHTML = x;
-}
+  }
