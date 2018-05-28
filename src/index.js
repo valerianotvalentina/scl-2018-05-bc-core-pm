80b0dced7a5e3bf334e54ea5f4960def36a70c02
Mi Nombre <valeriadroguettl@gmail.com>
2018-05-28 17:19:32 -0400

    Está igual.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 function clickCifrar() {
     //obtengo el desplazamiento
-    var desplazar = document.getElementById("desplazamiento").value;
+    const desplazar = document.getElementById("desplazamiento").value;
     //obtengo el mensaje
-    var mensaje = document.getElementById("escribeMensaje").value;
+    const mensaje = document.getElementById("escribeMensaje").value;
     //Utilizo la libreria cipher para codificar el mensaje
-    var mensajeCifrado = cipher.encode(mensaje, desplazar);
+    const mensajeCifrado = cipher.encode(mensaje, desplazar);
     //El mensaje encriptado lo muestro en el elemento "showMessage"
     document.getElementById("muestraMensaje").value = mensajeCifrado;
 
@@ -12,11 +12,11 @@ function clickCifrar() {
 
 function clickDescifrar() {
     //obtengo el desplazamiento
-    var desplazar = document.getElementById("desplazamiento").value;
+    const desplazar = document.getElementById("desplazamiento").value;
     //obtengo el mensaje
-    var mensaje = document.getElementById("escribeMensaje").value;
+    const mensaje = document.getElementById("escribeMensaje").value;
     //utilizo la libreria cipher para decodificar el mensaje
-    var mensajeDescifrado = cipher.decode(mensaje, desplazar);
+    const mensajeDescifrado = cipher.decode(mensaje, desplazar);
     //El mensaje encriptado lo muestro en el elemento "getMessage"
     document.getElementById("muestraMensaje").value = mensajeDescifrado;
 
